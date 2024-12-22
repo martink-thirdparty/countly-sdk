2fd02ef161679f771940b946d4ea128bafb89784
martin-kudlicka <email@martink.cz>
2024-12-22 20:48:49 +0100

    * Ninja build release configuration build fix

--- build-release_lib.cmd
+++ build-release_lib.cmd
@@ -1,7 +1,7 @@
 robocopy "%MkBuildPath%\output\Release Lib\x86\openssl\include\openssl" vanilla\src\openssl /MIR
 
 cmake -S vanilla -B %MkBuildPath%\intermediate\countly_sdk\release_lib
-cmake -DBUILD_SHARED_LIBS:BOOL="0" -DCMAKE_INSTALL_PREFIX:PATH="%MkBuildPath%\output\Release Lib\x86\countly_sdk" -DOPENSSL_ROOT_DIR:PATH="%MkBuildPath%\output\Release Lib\x86\openssl" -DOPENSSL_INCLUDE_DIR:PATH="%MkBuildPath%\output\Release Lib\x86\openssl\include" %MkBuildPath%\intermediate\countly_sdk\release_lib
+cmake -DBUILD_SHARED_LIBS:BOOL="0" -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_INSTALL_PREFIX:PATH="%MkBuildPath%\output\Release Lib\x86\countly_sdk" -DOPENSSL_ROOT_DIR:PATH="%MkBuildPath%\output\Release Lib\x86\openssl" -DOPENSSL_INCLUDE_DIR:PATH="%MkBuildPath%\output\Release Lib\x86\openssl\include" %MkBuildPath%\intermediate\countly_sdk\release_lib
 cmake --build %MkBuildPath%\intermediate\countly_sdk\release_lib --config Release
 rem does nothing: cmake --install %MkBuildPath%\intermediate\countly_sdk\release_lib --config Release
 
